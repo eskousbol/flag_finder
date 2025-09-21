a26c906023492c41b61a23eb110cd1dda2252b56
Emily Skousbol <skousbol@ualberta.ca>
2025-09-21 13:47:22 -0400

    chore: adding packages

--- flag_finder_service/Program.cs
+++ flag_finder_service/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static Tensorflow.Binding;
+using static Tensorflow.KerasApi;
+using Tensorflow;
+using Tensorflow.NumPy;
